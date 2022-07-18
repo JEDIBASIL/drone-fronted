1dcb11077a9cba805cdca6d932a75d884e50baad
Jedi <jedidiahbasil@gmail.com>
2022-07-18 12:33:02 +0100

    main website page

---
+++ src/pages/drone/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Hero } from '../../components/drone/Hero'
+import Service from '../../components/drone/Service'
+
+const Home = () => {
+  return (
+    <>
+   <Hero/>
+   <Service/>
+   </>
+  )
+}
+
+export default Home
